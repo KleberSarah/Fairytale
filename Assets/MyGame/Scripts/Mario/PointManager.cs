ef9cb3907150e07e62ff2f761cb4316a71d0575f
KleberSarah <sarahkleber0@gmail.com>
2025-11-26 14:30:25 +0100

    Collectibles prototyp

--- Assets/MyGame/Scripts/Mario/PointManager.cs
+++ Assets/MyGame/Scripts/Mario/PointManager.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PointManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TMP_Text timeText;
+    [SerializeField] private Slider silder;
+    [SerializeField] private GameObject acorn;
+    private float timeRemaining = 30;
+    private int points;
+    
+
+    private void Update()
     {
+        if(timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            timeText.text = "Time: " + Mathf.CeilToInt(timeRemaining).ToString();
+        }
+        else
+        {
+            timeText.text = "0";
+        }
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddPoints()
     {
-        
+        silder.value += 0.1f;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Acorn"))
+        {
+            AddPoints();
+            Destroy(other.gameObject);
+        }
+    }
+
 }
