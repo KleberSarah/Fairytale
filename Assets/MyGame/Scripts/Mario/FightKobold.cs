546b3203a44af47292fef35b6ac7b0e64593c8c6
ValiStein <valerie.steiner07@htl-salzburg.ac.at>
2026-01-28 13:53:06 +0100

    Startcommit28_01

--- Assets/MyGame/Scripts/Mario/FightKobold.cs
+++ Assets/MyGame/Scripts/Mario/FightKobold.cs
@@ -1,36 +1,76 @@
 using UnityEngine;
+
 using UnityEngine.UI;
+
 using TMPro;
 
+
+
 public class FightKobold : MonoBehaviour
+
 {
+
     [SerializeField] private Slider lifeSlider;
+
     [SerializeField] private Slider mySlider;
+
     [SerializeField] private TMP_Text loseWinText;
 
-    
+    public LevelFinisher levelFinisher;
+
+
+
+
+
+
 
     private void Start()
-    {
-        mySlider.value = PointManager.Instance.points;
-    }
-    public void FightPoints()
+
     {
 
-        lifeSlider.value -= mySlider.value ;
+        mySlider.value = PointManager.Instance.points;
+
+    }
+
+    public void FightPoints()
+
+    {
+
+
+
+        lifeSlider.value -= mySlider.value;
+
         mySlider.value = 0f;
 
+
+
         if (lifeSlider.value <= 0)
+
         {
+
             Debug.Log("Kobold besiegt!");
+
             loseWinText.gameObject.SetActive(true);
+
             loseWinText.text = "Du hast gewonnen!";
+
+            levelFinisher.CompleteLevel();
+
         }
+
         else
+
         {
+
             Debug.Log("Kobold hat noch Leben übrig: " + lifeSlider.value);
+
             loseWinText.gameObject.SetActive(true);
+
             loseWinText.text = "Du hast verloren!";
+            Invoke("ReloadLevel", 2f);
+
         }
+
     }
+
 }
