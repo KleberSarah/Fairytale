be870ce8c4dcf27aef3e6f30c91bd66be8f9cd07
ValiStein <valerie.steiner07@htl-salzburg.ac.at>
2026-04-08 15:47:59 +0200

    finish

---
+++ Assets/MyGame/Scripts/EndGame.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{    // Start is called once before the first execution of Update after the MonoBehaviour is created void Start
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
